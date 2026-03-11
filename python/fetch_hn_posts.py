5c3e2bc3577f26804873d44895a61f883a9d86ff
ParisaSolouki <parisasoluki2@gmail.com>
2026-03-11 17:21:24 +0100

    add gitignore for env

---
+++ python/fetch_hn_posts.py
@@ -0,0 +1,9 @@
+# Standard library
+import os
+from datetime import datetime, timezone
+
+# Third-party libraries
+import requests
+import pandas as pd
+from dotenv import load_dotenv
+import mysql.connector‌
