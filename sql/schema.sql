c396bcab9b3a24ead47754cddf82f889bfd530a5
ParisaSolouki <parisasoluki2@gmail.com>
2026-03-21 15:42:23 +0100

    SQL: create github_repos table

--- sql/schema.sql
+++ sql/schema.sql
@@ -1,4 +1,3 @@
-# %%
 -- =====================================
 -- Schema: Hacker News & GitHub Tracker
 -- =====================================
@@ -6,3 +5,21 @@
 CREATE DATABASE IF NOT EXISTS hn_github_tracker;
 
 USE hn_github_tracker;
+
+
+-- =====================================
+-- 1) Table: github_repos
+-- =====================================
+
+CREATE TABLE IF NOT EXISTS github_repos (
+  repo_id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  full_name VARCHAR(200) NOT NULL UNIQUE,
+  owner VARCHAR(100) NOT NULL,
+  repo VARCHAR(100) NOT NULL,
+  stars INT UNSIGNED,
+  language VARCHAR(50),
+  forks INT UNSIGNED,
+  open_issues INT UNSIGNED,
+  updated_at DATETIME,
+  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
