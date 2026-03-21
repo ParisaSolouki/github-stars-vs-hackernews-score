4d9ddb386604ebebeb3eed6a4f84496afe7e1f12
ParisaSolouki <parisasoluki2@gmail.com>
2026-03-21 15:41:45 +0100

    SQL: initialize database (hn_github_tracker)

---
+++ sql/schema.sql
@@ -0,0 +1,8 @@
+# %%
+-- =====================================
+-- Schema: Hacker News & GitHub Tracker
+-- =====================================
+
+CREATE DATABASE IF NOT EXISTS hn_github_tracker;
+
+USE hn_github_tracker;
